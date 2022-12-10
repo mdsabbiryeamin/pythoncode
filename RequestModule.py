3a61db4fe0bf1e050ee76149283712ae4cdae0fb
Yeamin Sabbir <mdyeaminsabbir608@gmail.com>
2022-12-10 23:47:59 +0600

    new file new code

---
+++ RequestModule.py
@@ -0,0 +1,18 @@
+# import requests
+# r = requests.get('https://jsonplaceholder.typicode.com/posts')
+# jsonfile = r.json()
+# jsonfile[0]='yeamin'
+# print(jsonfile)
+
+
+
+#### User Location and IP Details....///
+# apiUrl= 'https://api.ipify.org?format=json'
+# api = requests.get(apiUrl)
+# st=api.status_code
+# data =api.json()
+# ip=data.get('ip')
+# ip_location =f'https://ipapi.co/{ip}/json/'
+# location =requests.get(ip_location)
+# print(location.json())
+
