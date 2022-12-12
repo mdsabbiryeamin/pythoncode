7886f7bb103b1ba74bb6d13448240f49063b5a59
Yeamin Sabbir <mdyeaminsabbir608@gmail.com>
2022-12-12 11:00:59 +0600

    new code

--- weather_Data_Api.py
+++ weather_Data_Api.py
@@ -1,7 +1,11 @@
 import requests
-import pprint
 city_name='Dhaka'
 API_KEY='dda94182ee1218053ebd78f87d01bf95'
-weather_url= f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}'
+weather_url= f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric'
 requs=requests.get(weather_url)
-pprint.pprint(requs.json())
+jsonData =requs.json()
+
+city =jsonData.get('name')
+country =jsonData.get('sys').get('country')
+tem =jsonData.get('main').get('temp')
+print(f'weather City Name: {city}, weather country Name: {country}, weather tem : {tem}')
